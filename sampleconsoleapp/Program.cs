d977f2ad9e61a2e24da916131fcfc1523e412646
Aakash Pokkanayil <aakashpokkanayil@gmail.com>
2022-04-23 16:03:26 +0530

    sample commit

--- sampleconsoleapp/Program.cs
+++ sampleconsoleapp/Program.cs
@@ -1,3 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
 
+namespace sampleconsoleapp {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Hello Wrold");
+        }
+    }
+}
